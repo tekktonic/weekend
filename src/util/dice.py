7fdd0dfae235e0ffdc1aa7fe8f88242548353aea
Daniel Wilkins <tekk@linuxmail.org>
2021-10-24 14:27:30 -0400

    rename dice to roll to save a bunch of typing on my part

--- src/util/dice.py
+++ src/util/dice.py
@@ -1,10 +1,10 @@
 import random
 
-def dice(dice_num, dice_type):
+def roll(dice_num, dice_type):
     roll_total = 0
 
     for i in range(dice_num):
-        roll = roll.randint(1, int(dice_type))
+        roll = random.randint(1, int(dice_type))
         roll_total += roll
 
     return roll_total
