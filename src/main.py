07a9824d27144b4963d82e5eb9f120299e27fd0d
Daniel Wilkins <tekk@linuxmail.org>
2021-10-24 01:06:30 -0400

    Allow quitting

--- src/main.py
+++ src/main.py
@@ -27,11 +27,15 @@ def main():
     while running:
         dt = clock.tick(60)
         screen.fill((0, 255, 255))
-        running = controller.step(pygame.event.get(), dt)
+        events = pygame.event.get()
+        maybe_quit = [event for event in events if event.type == pygame.QUIT]
+        if len(maybe_quit) != 0:
+            break
+        running = controller.step(events, dt)
         pygame.display.flip()
     
     pygame.display.quit()
-    
+    pygame.quit()
 if __name__ == '__main__':
     main()
 
