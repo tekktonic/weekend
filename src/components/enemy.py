2fe4f469b5dad4719fc1d5665aa6331b7670030e
Daniel Wilkins <tekk@linuxmail.org>
2021-10-24 03:17:46 -0400

    Add player and enemy tags

---
+++ src/components/enemy.py
@@ -0,0 +1,5 @@
+from util.entity import Component
+
+class Enemy(Component):
+    def __init__(self, s, i, d):
+        super().__init__()
---
+++ src/components/player.py
@@ -0,0 +1,5 @@
+from util.entity import Component
+
+class Player(Component):
+    def __init__(self, s, i, d):
+        super().__init__()
