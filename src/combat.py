1536caa10f44c9529038bf1a0eda5c08cc2c5a09
Daniel Wilkins <tekk@linuxmail.org>
2021-10-24 02:19:21 -0400

    Test draw the font

--- src/combat.py
+++ src/combat.py
@@ -1,16 +1,23 @@
 import sys
+
+import pygame
+
 from util.spritesheet import SpriteSheet
+import util.resources as resources
 
 class Module():
     def __init__(self, screen):
         self.screen = screen
         self.sprite = SpriteSheet("so", 16, 16)
-        self.count = 1000
         self.frame = 0
-    
+        self.text = pygame.font.Font(resources.font_path("Bitmgothic"), 16).render('But I Want To Be The King', False, (0, 0, 0))
+        self.anim_time = 20
+                
     def step(self, events, dt):
-        self.count -= 1
-        if (self.count % 10 == 1):
+        self.anim_time -= dt
+        if self.anim_time <= 0:
             self.frame = (self.frame + 1) % 4
+            self.anim_time = 20
+        self.screen.blit(self.text, (64, 64))
         self.screen.blit(self.sprite.backing_image, (160, 100), self.sprite.get((self.frame, 0)))
-        return self.count > 0
+        return True
